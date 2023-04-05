e32aa513f7d1478b61a1e12cb63ec8365a73da95
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-04-05 16:30:44 +0500

    creation and partially configured API

--- hcs_system_project/api/v1/urls.py
+++ hcs_system_project/api/v1/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views
+
 from .hcs_system.views import HomeView, CreatingApplicationView
+from .hcs_systemAPI.views import RegisterView
+
 
 app_name = "v1inc"
 urlpatterns = [
     path('account/', HomeView.as_view(), name="home"),
     path('request/', CreatingApplicationView.as_view(), name="request"),
+
+    # hcs_systemAPI
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', views.LoginView.as_view(template_name='auth_users/login_users.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- hcs_system_project/config/settings.py
+++ hcs_system_project/config/settings.py
@@ -111,6 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
